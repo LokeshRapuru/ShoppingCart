12ba20e27b4db7e281ea98b1de43385988d997f9
VaibhavBade <VaibhavBade@LAPTOP-ACLHRS82>
2021-02-03 13:01:01 +0530

    adding test class

--- my-shopping-cart/src/test/java/com/ind/shopping/MyShoppingCartApplicationTests.java
+++ my-shopping-cart/src/test/java/com/ind/shopping/MyShoppingCartApplicationTests.java
@@ -3,11 +3,16 @@ package com.ind.shopping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ind.shopping.vaibhav.model.MultiplyNo;
+
 @SpringBootTest
 class MyShoppingCartApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		
+		
 	}
 
 }
