499f4891696135094194ef78e70c0a1eedaa7cec
AakashPawar <AakashPawar@LAPTOP-063BO803>
2021-02-05 10:15:50 +0530

    added maxnum class and test case maxnum class

---
+++ my-shopping-cart/src/test/java/com/ind/shopping/aakash/testCase_Maxnum.java
@@ -0,0 +1,14 @@
+package com.ind.shopping.aakash;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class testCase_Maxnum {
+	@Test
+	public void testFindMax(){
+	MaxNum maxnum=new MaxNum();
+	assertEquals(4,maxnum.findMax(new int[]{1,3,4,2}));
+
+}
+}
